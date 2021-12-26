5ec6e9548c312dc15e4278a5de205a22ea4bfaaf
PratikshaKate <pratikshakate.04@gmail.com>
2021-12-26 21:39:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "view"));
+app.set("views", path.join(__dirname, "View"));
 
 app.get("/", (req, resp, next) => {
   resp.render("index");
